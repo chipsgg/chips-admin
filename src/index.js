e07610e136e21c47c7a227216d27d58e9f3f5e98
Jacob Dent <jdent@tacyarg.com>
2019-04-27 20:25:51 -0500

    pods

--- src/index.js
+++ src/index.js
@@ -10,13 +10,13 @@ import Authenticate from './libs/authenticate'
 import Layout from "./Layout";
 
 const main = async () => {
-  let actions = await Actions("http://149.28.121.92:9000");
+  let actions = await Actions("https://api.chips.gg");
 
   const user = await Authenticate(actions)
   console.log(user)
 
   // kinda hacky
-  const auth = Auth("http://149.28.121.92:9001");
+  const auth = Auth("https://auth.chips.gg");
   actions = {...actions, ...auth}
 
   return ReactDOM.render(
