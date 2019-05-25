3c737a4f5985ac96eddbc649b15e51f14f9d37b7
Jacob Dent <jdent@tacyarg.com>
2019-05-25 11:26:26 -0500

    set to demo api

--- src/index.js
+++ src/index.js
@@ -10,13 +10,16 @@ import Authenticate from './libs/authenticate'
 import Layout from "./Layout";
 
 const main = async () => {
-  let actions = await Actions("https://api.chips.gg");
+  // let actions = await Actions("https://api.chips.gg");
+  let actions = await Actions("http://35.222.81.236:9991");
 
   const user = await Authenticate(actions)
   console.log(user)
 
   // kinda hacky
-  const auth = Auth("https://auth.chips.gg");
+  // const auth = Auth("https://auth.chips.gg");
+  const auth = Auth("http://35.222.81.236:9993");
+
   actions = {...actions, ...auth}
 
   return ReactDOM.render(
