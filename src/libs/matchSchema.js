267d8d0f25dede1be247da954e699d364677ef29
tacyarg <jdent@tacyarg.com>
2019-05-27 23:05:13 +0000

    match editor

---
+++ src/libs/matchSchema.js
@@ -0,0 +1,85 @@
+export default {
+  draw: false,
+  game: "csgo",
+  games: [
+    {
+      finished: false,
+      id: "6621",
+      map: null,
+      position: 1,
+      startTime: null,
+      winner: null
+    }
+  ],
+  id: "csgo-544559",
+  league: {
+    id: "4152",
+    image:
+      "https://cdn.pandascore.co/images/league/image/4152/600px-DreamHack_Black-RGB.png",
+    name: "DreamHack",
+    url: null
+  },
+  matchType: "best_of",
+  name: "fnatic vs Cloud9",
+  opponents: [
+    {
+      id: "3217",
+      image: "https://cdn.pandascore.co/images/team/image/3217/FNATIC.png",
+      name: "fnatic"
+    },
+    {
+      id: "3223",
+      image: "https://cdn.pandascore.co/images/team/image/3223/CLOUD_9.png",
+      name: "Cloud9"
+    }
+  ],
+  results: [
+    { score: 0, team: "Cloud9", teamid: "3223" },
+    { score: 0, team: "fnatic", teamid: "3217" }
+  ],
+  rulesets: [
+    {
+      draw: false,
+      name: "Overall Winner",
+      outcome: null,
+      position: 0,
+      selections: ["fnatic", "Cloud9"],
+      state: "open"
+    },
+    {
+      forfeit: null,
+      name: "Game 1 Winner",
+      outcome: null,
+      position: 1,
+      selections: ["fnatic", "Cloud9"],
+      state: "open"
+    },
+    {
+      forfeit: false,
+      name: "Game 1 Round Limit",
+      outcome: null,
+      position: 1,
+      selections: ["fnatic", "Cloud9"],
+      state: "open"
+    },
+    {
+      forfeit: false,
+      name: "Game 1 Pistol Round Winner First Half",
+      outcome: null,
+      position: 1,
+      selections: ["fnatic", "Cloud9"],
+      state: "open"
+    },
+    {
+      forfeit: false,
+      name: "Game 1 Pistol Round Winner Second Half",
+      outcome: null,
+      position: 1,
+      selections: ["fnatic", "Cloud9"],
+      state: "open"
+    }
+  ],
+  startTime: "2019-05-28T21:00:00Z",
+  state: "not_started",
+  winner: null
+};
