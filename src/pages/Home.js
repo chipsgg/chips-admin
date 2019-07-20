e45ebb1b8d34bdcf6846aaf587b39a2afef346cc
tacyarg <jdent@tacyarg.com>
2019-07-20 11:33:23 -0500

    date selection for stats

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,24 +1,93 @@
 import React, { useState, useEffect } from "react";
 
-import { Pane, Text, Heading } from "evergreen-ui";
+import { Pane, Text, Heading, TextInputField } from "evergreen-ui";
 import Stat from "../components/Stat.js";
+
+const DateRange = ({ onChange }) => {
+  const [startDate, setStartDate] = useState("");
+  const [endDate, setEndDate] = useState("");
+
+  useEffect(() => {
+    onChange({
+      start: isNaN(Date.parse(startDate)) ? 1 : Date.parse(startDate),
+      end: isNaN(Date.parse(endDate)) ? Date.now() : Date.parse(endDate)
+    });
+  }, [startDate, endDate]);
+
+  return (
+    <Pane display="flex">
+      <TextInputField
+        type="date"
+        label="Start Date"
+        value={startDate}
+        onChange={e => {
+          setStartDate(e.target.value);
+        }}
+      />
+      <Pane marginLeft={4} marginRight={4} />
+      <TextInputField
+        type="date"
+        label="End Date"
+        value={endDate}
+        onChange={e => {
+          setEndDate(e.target.value);
+        }}
+      />
+    </Pane>
+  );
+};
 
 const Home = ({ actions }) => {
   const [stats, setStats] = useState({});
 
-  useEffect(() => {
-    actions
-      .getStats({
-        start: 1,
-        end: Date.now()
-      })
-      .then(([stats]) => {
-        console.log(stats);
-        setStats(stats);
+  const LoadStats = timerange => {
+    // timerange = {
+    //   start: 1,
+    //   end: Date.now()
+    // }
+
+    return actions
+      .getStats(timerange)
+      .then(timeSeries => {
+        const results = timeSeries.reduce((memo, stats, idx) => {
+          console.log(idx, stats);
+
+          // for each stat in the time series, we add them together
+          const keys = Object.keys(stats);
+          keys.forEach(key => {
+            if (key == "id" || key == "created" || key == "updated") return;
+            memo[key] = 0;
+            memo[key] += stats[key];
+          });
+
+          return memo;
+        }, {});
+
+        console.log(results);
+
+        return setStats(results);
       })
       .catch(console.error);
-  }, []);
+  };
 
+  return (
+    <Pane width={"100%"} display="flex" flexDirection="column">
+      <Pane
+        width={"100%"}
+        borderBottom
+        display="flex"
+        padding={16}
+        background="tint1"
+        alignItems="center"
+      >
+        <DateRange onChange={LoadStats} />
+      </Pane>{" "}
+      <Stats {...stats} />
+    </Pane>
+  );
+};
+
+const Stats = stats => {
   return (
     <Pane
       width={"100%"}
